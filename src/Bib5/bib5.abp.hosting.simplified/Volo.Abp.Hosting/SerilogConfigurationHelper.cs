c7d16b5a681e0e2edf0103af5011c46aacbb45b7
xxmo@soludatech.com <moxx#123>
2025-01-07 09:33:06 +0800

    add seq log

--- src/Bib5/bib5.abp.hosting.simplified/Volo.Abp.Hosting/SerilogConfigurationHelper.cs
+++ src/Bib5/bib5.abp.hosting.simplified/Volo.Abp.Hosting/SerilogConfigurationHelper.cs
@@ -11,6 +11,7 @@ using Serilog.Events;
 using Serilog.Settings.Configuration;
 using Serilog.Sinks.File;
 using Serilog.Sinks.SystemConsole.Themes;
+using static Volo.Abp.Hosting.HostingConsts;
 
 namespace Volo.Abp.Hosting;
 
@@ -18,7 +19,6 @@ public static class SerilogConfigurationHelper
 {
     public static void Configure(string? appName = null)
     {
-        //IL_0090: Unknown result type (might be due to invalid IL or missing references)
         ConfigurationManager configurationManager = new ConfigurationManager();
         configurationManager.AddJsonFile("appsettings.json");
         configurationManager.AddEnvironmentVariables("ASPNETCORE_");
@@ -27,43 +27,42 @@ public static class SerilogConfigurationHelper
         {
             configurationManager.AddJsonFile("appsettings." + text + ".json");
         }
+
         IConfigurationRoot configurationRoot = configurationManager.AddEnvironmentVariables().AddCommandLine(Environment.GetCommandLineArgs()).Build();
         if (appName == null)
         {
             appName = configurationRoot["App:AppName"];
         }
+
         Check.NotNull<string>(appName, "appName");
-        LoggerConfiguration obj = LoggerConfigurationAsyncExtensions.Async(new LoggerConfiguration().MinimumLevel.Verbose().Enrich.FromLogContext().Enrich.WithProperty("Application", (object)(appName ?? ""), false).WriteTo, (Action<LoggerSinkConfiguration>)delegate (LoggerSinkConfiguration c)
+        LoggerConfiguration loggerConfiguration = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("Application", appName ?? "")
+            .Enrich.WithProperty("Environment", text)
+            .WriteTo.Async(delegate (LoggerSinkConfiguration c)
         {
-            ConsoleLoggerConfigurationExtensions.Console(c, (LogEventLevel)0, "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}", (IFormatProvider)null, (LoggingLevelSwitch)null, (LogEventLevel?)null, (ConsoleTheme)null, false, (object)null);
-        }, 10000, false);
+            c.Console();
+        });
         string path = ApplicationEnvironmentHelper.GetLogRoot(appName);
         if (!ApplicationEnvironmentHelper.IsSetLogRoot())
         {
             path = Path.Combine(path, "logs");
         }
+
         string logFile = Path.Combine(path, appName.ToLowerInvariant() + ".log");
-        LoggerConfigurationAsyncExtensions.Async(obj.WriteTo, (Action<LoggerSinkConfiguration>)delegate (LoggerSinkConfiguration c)
+        loggerConfiguration.WriteTo.Async(delegate (LoggerSinkConfiguration c)
         {
-            FileLoggerConfigurationExtensions.File(
-                c,
-                logFile,
-                (LogEventLevel)0,
-                "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
-                (IFormatProvider)null,
-                (long?)1073741824L,
-                (LoggingLevelSwitch)null,
-                false,
-                false,
-                (TimeSpan?)null,
-                RollingInterval.Day,
-                true,  // 启用当文件达到限制大小时滚动
-                (int?)120,
-                (Encoding)null,
-                (FileLifecycleHooks)null,
-                (TimeSpan?)null);
-        }, 10000, false);
-        ConfigurationLoggerConfigurationExtensions.Configuration(obj.ReadFrom, (IConfiguration)configurationRoot, (ConfigurationReaderOptions)null);
-        Log.Logger = (ILogger)(object)obj.CreateLogger();
+            c.File(logFile, LogEventLevel.Verbose, "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}", null, 1073741824L, null, buffered: false, shared: false, null, RollingInterval.Day, rollOnFileSizeLimit: true, 120);
+        });
+
+        var seqUrl = configurationRoot["Seq:Url"];
+        if (!string.IsNullOrEmpty(seqUrl))
+        {
+            loggerConfiguration.WriteTo.Seq(seqUrl);
+        }
+
+        loggerConfiguration.ReadFrom.Configuration(configurationRoot);
+        Log.Logger = loggerConfiguration.CreateLogger();
     }
 }
